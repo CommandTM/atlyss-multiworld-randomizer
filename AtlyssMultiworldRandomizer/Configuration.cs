3529c1b9290616f29128cc70a7d7cb116a134f9b
CommandTM <commandnoobmc23@gmail.com>
2025-10-20 18:56:07 -0400

    Basic connection

---
+++ AtlyssMultiworldRandomizer/Configuration.cs
@@ -0,0 +1,19 @@
+﻿using BepInEx.Configuration;
+
+namespace AtlyssMultiworldRandomizer;
+
+public class Configuration
+{
+    public static ConfigEntry<string> connectionIP { get; private set; }
+    public static ConfigEntry<string> connectionPort { get; private set; }
+    public static ConfigEntry<string> connectionSlot { get; private set; }
+    public static ConfigEntry<string> connectionPassword { get; private set; }
+
+    public static void CreateConfigEntries(ConfigFile config)
+    {
+        connectionIP = config.Bind(MyPluginInfo.PLUGIN_GUID, "localhost", "IP used to connect to the multiworld");
+        connectionPort = config.Bind(MyPluginInfo.PLUGIN_GUID, "38281", "Port used to connect to the multiworld");
+        connectionSlot = config.Bind(MyPluginInfo.PLUGIN_GUID, "", "Slot name used to connect to the multiworld");
+        connectionPassword = config.Bind(MyPluginInfo.PLUGIN_GUID, "", "Password to connect to the multiworld");
+    }
+}
--- AtlyssMultiworldRandomizer/Plugin.cs
+++ AtlyssMultiworldRandomizer/Plugin.cs
@@ -1,4 +1,8 @@
-﻿using BepInEx;
+﻿
+using System;
+using Archipelago.MultiClient.Net;
+using Archipelago.MultiClient.Net.Enums;
+using BepInEx;
 using BepInEx.Logging;
 
 namespace AtlyssMultiworldRandomizer;
@@ -7,11 +11,57 @@ namespace AtlyssMultiworldRandomizer;
 public class Plugin : BaseUnityPlugin
 {
     internal static new ManualLogSource Logger;
+    public static ArchipelagoSession Session;
+    public static LoginResult LoginResult;
+    public static LoginSuccessful LoginSuccessful;
 
     private void Awake()
     {
         // Plugin startup logic
         Logger = base.Logger;
         Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+
+        Configuration.CreateConfigEntries(Config);
+        On.CharacterSelectManager.Select_CharacterFile += _connectionMultiworld;
+        On.OptionsMenuCell.SaveQuit_Game += _disconnectionMultiworld;
+    }
+
+    private void _disconnectionMultiworld(On.OptionsMenuCell.orig_SaveQuit_Game orig, OptionsMenuCell self)
+    {
+        Session.Socket.DisconnectAsync();
+        Logger.LogInfo("Disconnected from multiworld...");
+        orig(self);
+    }
+
+    private void _connectionMultiworld(On.CharacterSelectManager.orig_Select_CharacterFile orig, CharacterSelectManager self)
+    {
+        Session = ArchipelagoSessionFactory.CreateSession(
+            $"{Configuration.connectionIP.Value}:{Configuration.connectionPort.Value}");
+        
+        try
+        {
+            LoginResult = Session.TryConnectAndLogin(
+                "Atlyss",
+                Configuration.connectionSlot.Value,
+                ItemsHandlingFlags.AllItems,
+                new Version(0, 6, 3),
+                password: Configuration.connectionPassword.Value);
+        }
+        catch (Exception ex)
+        {
+            Logger.LogError("There was an error while connecting to the multiworld!");
+            var result = new LoginFailure(ex.GetBaseException().Message);
+            foreach (var error in result.Errors)
+            {
+                Logger.LogError(error);
+            }
+        }
+
+        if (LoginResult.Successful)
+        {
+            LoginSuccessful = (LoginSuccessful)LoginResult;
+            Logger.LogInfo($"Successfully connected to multiworld! Slot: {LoginSuccessful.Slot}");
+            orig(self);
+        }
     }
 }
